a8dd255d78f23f5235f379fb2b55f653c23b26f1
Seraphond <seraphond@localhost.localdomain>
2016-09-16 14:29:29 +0200

    pause

--- TDTP1/Maccess.c
+++ TDTP1/Maccess.c
@@ -3,25 +3,33 @@
 #include<limits.h>
 #include<unistd.h>
 #include<string.h>
-int main(int argc,char*argv[]){
-  char * nom_fic =argv[2];
-  int status;
-  if(argc>3){
-    printf("Erreur de syntaxe. Usage :maccess <mode> <fichier>\n");
-    exit(1);
-  }
-  if(strcmp(argv[1],"-x")==0){
-    status=access(nom_fic,X_OK);
-    
-  }else if(strcmp(argv[1],"-r")==0){
-    status=access(nom_fic,R_OK);
-    }else if(strcmp(argv[1],"-w")==0){
-    status=access(nom_fic,W_OK);
-  }else{
-    printf("J\'ai pas capte le mode désolé\n");
-    exit(1);
-  }
+#include<errno.h>
 
+
+int main(int argc,char*argv[]){
+  int errnum;
+  int status;
+  if(argc>4){
+    perror("Errreur de syntaxe , trop d'arguments");
+    exit(1);
+  }else if(strcmp(argv[3],"-v")==0){
+    
+
+  }else{
+    char * nom_fic =argv[2];
+    
+
+    if(strcmp(argv[1],"-x")==0){
+      status=access(nom_fic,X_OK);
+    
+    }else if(strcmp(argv[1],"-r")==0){
+      status=access(nom_fic,R_OK);
+    }else if(strcmp(argv[1],"-w")==0){
+      status=access(nom_fic,W_OK);
+    }else{
+      perror("J\'ai pas capté le mode désolé bro\n");
+      exit(2);
+    }
   if(status){
     printf("tu n'a pas le droit\n");
     
