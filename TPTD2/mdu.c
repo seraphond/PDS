ab30daa439bb7a395509520364beb98a57349f58
seraphond <erwan.sorgniard@gmail.com>
2016-09-23 11:00:27 +0200

    td2

---
+++ TPTD2/mdu.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "string.h"
+
+
+int du_file(const char *pathname){
+    /* Fichier Regulier */
+
+    struct stat st;
+    int status;
+    status=status(pathname,&st);
+
+  return 0;
+}
+int main(int argc,char* argv[]){
+
+
+    return 0;
+}
+
