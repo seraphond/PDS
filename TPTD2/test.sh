32345e86bd51728ab549c2a65fc3d8987fc1209c
seraphond <erwan.sorgniard@gmail.com>
2016-09-26 09:14:05 +0200

    le bon cette fois

--- TPTD2/test.sh
+++ TPTD2/test.sh
@@ -5,16 +5,17 @@ echo '#commande origininale' &>>mdu_log.txt
 
 du -B 512 -L mdu.c &>> mdu_log.txt
 
-echo'#commande mdu' &>> mdu_log.txt
+echo '#commande mdu' &>> mdu_log.txt
 
 ./mdu.beta -L mdu.c &>> mdu_log.txt
 
 echo '#commande originale avec option -b' &>>mdu_log.txt
 
-du -b mdu.c
+du -b mdu.c &>> mdu_log.txt
 
 echo '#commande mdu avec option -b ' &>>mdu_log.txt
 
+./mdu.beta -b mdu.c &>> mdu_log.txt
 
 
 #les commandes sont bien identiques
