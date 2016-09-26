268be8e8a91bb5fdbbbf3e7755da61ac7d79f82e
seraphond <erwan.sorgniard@gmail.com>
2016-09-26 08:58:53 +0200

    test.sh

---
+++ TPTD2/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo '#commande origininale' &>>mdu_log.txt
+
+du -B 512 -L mdu.c &>> mdu_log.txt
+
+echo'#commande mdu' &>> mdu_log.txt
+
+./mdu.beta -L mdu.c &>> mdu_log.txt
+
+echo '#commande originale avec option -b' &>>mdu_log.txt
+
+du -b mdu.c
+
+echo '#commande mdu avec option -b ' &>>mdu_log.txt
+
+
+
+#les commandes sont bien identiques
