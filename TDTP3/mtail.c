572d4cd4ed41c04576de0dc17f4f39f37a81300b
seraphond <erwan.sorgniard@gmail.com>
2016-09-30 14:02:17 +0200

    pwd

--- TDTP3/mtail.c
+++ TDTP3/mtail.c
@@ -5,15 +5,27 @@
 #include <sys/stat.h>
 #include <sys/type.h>
 
+static  int buffer=1024;
 
+easyMtail(char* path,int ligne){
+    
+}
 
-mtail(char * path, int n){
+mtail(char * path, int lignes){
     FILE* file=NULL;
-    int buffer=n;
+
     int nb_ligne=0;
     char *contenu_ligne;
     file=open(path,O_RDONLY);
     lseek(file,-buffer,SEEK_END);
+    while((contenu_ligne=fgets())!=NULL){
+        nb_ligne++;
+    }
+    if(nb_ligne>lignes){
+        lseek(file,-buffer,SEEK_END);
+
+
+    }
 
 
 
