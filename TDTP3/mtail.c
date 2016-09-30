b9f806ae29eb4c098084cc82c0127c0ee842ffda
seraphond <erwan.sorgniard@gmail.com>
2016-09-30 13:28:37 +0200

    pwd

---
+++ TDTP3/mtail.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/stat.h>
+#include <sys/type.h>
+
+static int taille_buffer=1024;
+
+mtail(char * path, nb_line n){
+
+}
+
+int main(int argc,char* argv[]){
+
+
+    return 0;
+}
+
