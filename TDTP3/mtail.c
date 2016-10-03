846b6280d57347760451340570222b166472b282
seraphond <erwan.sorgniard@gmail.com>
2016-10-03 15:11:06 +0200

    pwd

--- TDTP3/mtail.c
+++ TDTP3/mtail.c
@@ -10,7 +10,7 @@
 
 /*write(stdout_fileno,buffer+i+1,bufsize-i-1)*/
 
-int compteur_de_ligne(char* buf,int buffer_size){
+int compteur_de_ligne(unsigned  char* buf,int buffer_size){
     int i=0;
     int nb_ligne=0;
     while (i<buffer_size) {
@@ -19,44 +19,56 @@ int compteur_de_ligne(char* buf,int buffer_size){
         }
         i++;
     }
-    return i;
+    return nb_ligne;
 }
 
+off_t find_offset(int fd,int ligne_obj) {
+    int nb_ligne = 0;
+    char *c[1];
+    off_t obj=0;
+
+    while (read(fd, c, 1) != 0) {
+        printf("%s",c[0]);
+        if (nb_ligne == ligne_obj) {
+            return  lseek(fd, obj, SEEK_CUR);
+
+        }
+        if (c[0] == '\n') {
+            nb_ligne++;
+        }
+    }
+    return 0;
+}
 void easyMtail(char* path,int ligne){
-    int file;
-    int nb_ligne=0;
-    char *contenu=NULL;
-    int buf_size_real;
-    file = open(path,O_RDONLY);
-    printf("file = %d\n",file);
-    buf_size_real = read(file,contenu,BUFFER_SIZE);
-    printf("Real buf size = %d",buf_size_real);
-    nb_ligne=compteur_de_ligne(contenu, buf_size_real);
-    printf("%d\n",nb_ligne);
+
 }
 
 /*void mtail(char * path, int lignes){
-    FILE* file=NULL;
 
-    int nb_ligne=0;
-    char *contenu_ligne;
-    file=open(path,O_RDONLY);
-    lseek(file,-buffer,SEEK_END);
-    while((fgets(contenu_ligne,buffer,file))!=NULL){
-        nb_ligne++;
-    }
-    if(nb_ligne>lignes){
-        lseek(file,-buffer,SEEK_END);
-
-
-    }
 }*/
 
 int main(int argc,char* argv[]){
-    char * nom_fic=argv[1];
-    int nb_lignes=(int)argv[2];
+    char * nom_fic;
+    int lignes;
+    lignes=0;
+    nom_fic=argv[1];
+    int debut;
+    debut=atoi(argv[2]);
+    int file;
+    unsigned char contenu[BUFFER_SIZE];
+    int octet_lu=0;
+    off_t offset;
 
-    easyMtail(nom_fic,nb_lignes);
+
+    file = open(nom_fic,O_RDONLY);
+
+
+    octet_lu = read(file,contenu,BUFFER_SIZE+1);
+    lignes=compteur_de_ligne(contenu, BUFFER_SIZE+1);
+    offset=find_offset(file,(lignes-debut));
+    //lseek(file,offset,SEEK_SET);
+    write(STDOUT_FILENO,contenu,BUFFER_SIZE);
+    //easyMtail(nom_fic,nb_lignes);
 
     return 0;
 }
