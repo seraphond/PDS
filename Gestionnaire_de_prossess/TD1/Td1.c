af4afdc3edb1428d068cad8fb487bb51d7de2240
seraphond <erwan.sorgniard@gmail.com>
2016-11-04 13:24:51 +0100

    truc

---
+++ Gestionnaire_de_prossess/TD1/Td1.c
@@ -0,0 +1,39 @@
+//
+// Created by seraphond on 04/11/16.
+//
+
+
+#define MAXJOB 16
+#define MAXLINE 16
+struct job_s{
+    pid_t job_pid;
+    int job_id;
+    int job_state;
+    char job_cmdline[MAXLINE];
+};
+
+static  struct job_s jobs[MAXJOB];
+
+int sigaction_wrapper(int signum,handler_t *handler){
+    struct  sigaction act;
+    act.sahandler=handler;
+    __sigemptyset(&act.samask);
+    act.sa_flays=  ;
+    if (sigaction(signum,&act,NULL)<0){
+        unix_error("error sigaction wrapper");
+
+    }
+
+    return 0;
+
+}
+
+void sigint(int sig){
+    pid_t pid;
+    if((pid=jobsfpid())>0){
+        if(kill(pid,sig)<0){
+            unix_error("");
+        }
+    }
+}
+
