36fa19c5de98c8d13d9c7402a392efe2463a143c
Thomas <arblay.thomas@gmail.com>
2016-11-25 16:31:47 +0100

    Commit de 16h30

--- Gestionnaire_de_prossess/TP1/mshell/pipe.c
+++ Gestionnaire_de_prossess/TP1/mshell/pipe.c
@@ -3,35 +3,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <wait.h>
 #include "pipe.h"
+#include <sys/wait.h>
 
 void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
 
+    int i,j;
+    int pipes[MAXCMDS][2];
+
+
+    for(i = 0; i < nbcmd; i++){
+        pipe(pipes[i]);
+    }
+    switch(fork()){
+        case -1:
+            perror("Erreur fork debut");
+            exit(EXIT_FAILURE);
+        case 0:
+            dup2(pipes[0][1],STDOUT_FILENO);
+            close(pipes[0][1]);
+            execvp(cmds[0][0],cmds[0]);
+        default:
+            break;
+    }
+    for (i = 1; i < nbcmd-1; i++) {
+        switch (fork()){
+            case -1:
+                perror("Erreur fork milieu");
+                exit(EXIT_FAILURE);
+            case 0:
+                dup2(pipes[i-1][0],STDIN_FILENO);
+                dup2(pipes[i][1],STDOUT_FILENO);
+                close(pipes[i-1][0]);
+                close(pipes[i][1]);
+                execvp(cmds[i][0],cmds[i]);
+            default:
+                break;
+        }
+    }
+    switch(fork()){
+        case -1:
+            perror("Erreur fork fin");
+            exit(EXIT_FAILURE);
+        case 0:
+            dup2(pipes[i][0],STDIN_FILENO);//ou i-1 si erreur index
+            close(pipes[i][0]);
+            execvp(cmds[nbcmd-1][0],cmds[nbcmd-1]);
+        default:
+            break;
+    }
+    for (j = 0; j < nbcmd; ++j) {
+        wait(NULL);
+    }
+    free(pipes);
+    return;
+}
+
+/*void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
 
     int pid;
     int fd[2];
 
-     pipe(fd);
+    pipe(fd);
 
 
     switch (fork()){
         case -1:
             perror("erreur fork");
             exit(EXIT_FAILURE);
-     case 0:
+        case 0:
             close(fd[0]);
             dup2(fd[1],STDOUT_FILENO);
             close(fd[1]);
-            printf("je passe ici");
             execvp(cmds[0][0],cmds[0]);
-
         default:
-
             break;
     }
 
-    switch (pid=fork()){
+    switch (fork()){
         case -1:
             perror("erreur fork");
             exit(EXIT_FAILURE);
@@ -41,7 +90,7 @@ void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
             close(fd[0]);
             execvp(cmds[1][0],cmds[1]);
         default:
-            ;
+            break;
     }
 
     close(fd[0]);
@@ -49,7 +98,5 @@ void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
     wait(NULL);
     wait(NULL);
 
-
-
     return;
-}
+}*/
